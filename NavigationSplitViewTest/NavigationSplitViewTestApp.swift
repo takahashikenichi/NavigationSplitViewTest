a781838a5207a492eff6a534d8a10a2c19d9385e
Kenichi Takahashi <takahashi.kenichi@gmail.com>
2022-07-05 00:59:16 +0900

    Initial Commit

---
+++ NavigationSplitViewTest/NavigationSplitViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSplitViewTestApp.swift
+//  NavigationSplitViewTest
+//
+//  Created by Kenichi Takahashi on 2022/07/05.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSplitViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
